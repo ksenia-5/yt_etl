d8ab558db714bfc13e6780b3dc27d9d024bee333
ksenia-5 <xenia.germanovich@gmail.com>
2025-11-17 09:18:12 +0000

    Added .env to .gitignore and savved yt API key in .env; added function to get playlist id for channel

---
+++ video_stats.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env')
+
+API_KEY = os.getenv('API_KEY')
+
+YT_HANDLE = 'wgsn'
+
+url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={YT_HANDLE}&key={API_KEY}'
+
+def get_playlist_id():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        data = response.json()
+
+        channel_items = data.get('items')[0]
+        channel_playlist_id = channel_items.get('contentDetails').get('relatedPlaylists').get('uploads')
+        print(channel_playlist_id)
+        return channel_playlist_id
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == '__main__':
+    get_playlist_id()
