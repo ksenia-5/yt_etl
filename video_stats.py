4e6ff655f03544edeceb361fb8223f4e8057118a
wgsn-germk <ksenia.germanovich@wgsn.com>
2025-11-18 08:21:50 +0000

    Added function to get playlist items from playlist id

--- video_stats.py
+++ video_stats.py
@@ -10,9 +10,11 @@ API_KEY = os.getenv('API_KEY')
 
 YT_HANDLE = 'wgsn'
 
-url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={YT_HANDLE}&key={API_KEY}'
+MAX_RESULTS = 50
 
-def get_playlist_id():
+
+def get_playlist_id() -> str:
+    url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={YT_HANDLE}&key={API_KEY}'
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -26,6 +28,43 @@ def get_playlist_id():
     
     except requests.exceptions.RequestException as e:
         raise e
+
+
+
+def get_playlist_items(playlist_id : str) -> list:
+
+    video_id_list = []
+    page_token = None
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={MAX_RESULTS}&playlistId={playlist_id}&key={API_KEY}'    
+
+    try:
+        while True:
+            url = base_url
+            if page_token:
+                url += f"&pageToken={page_token}"
+                print(url)
+            
+
+            response = requests.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+            playlist_items = data.get('items', [])
+            
+            new_video_ids = [pi.get('contentDetails').get('videoId') for pi in playlist_items]
+            video_id_list += new_video_ids
+
+            page_token = data.get('nextPageToken', None)
+            
+            if not page_token:
+                break
+        
+        return video_id_list
     
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == '__main__':
-    get_playlist_id()
+    playlist_id = get_playlist_id()
+    get_playlist_items(playlist_id)
+
